70b0fbd21a6b1e08319c35b9cd89dbe8b7c7241a
Hrushikesh <hrushigkesh@gmail.com>
2025-07-08 21:32:57 +0530

    Create basic GUI without interactions, Day 16

--- Todolist_app/gui16.py
+++ Todolist_app/gui16.py
@@ -1,6 +1,11 @@
+#Creating a Desktop GUI
 import functions
-import PySimpleGUI as sg
+import FreeSimpleGUI as sg
 
-window = sg.Window('My To-do App', layout=[[]])  # layout must be a list of lists
+label = sg.Text("Type a todo")
+input_box = sg.InputText(tooltip="Enter todo")
+button = sg.Button("Add")
+
+window = sg.Window('My To-do App', layout=[[label], [input_box, button]])  # layout must be a list of lists
 window.read()
 window.close()
