dbfb5a4c792ec16543a34d638e4be6a44ae4c3ae
Hrushikesh <hrushigkesh@gmail.com>
2025-07-09 08:38:54 +0530

    Implement Add Button

--- Todolist_app/gui17.py
+++ Todolist_app/gui17.py
@@ -10,7 +10,18 @@ window = sg.Window('My To-do App',
                    layout=[[label], [input_box, button]],
                    font = ('Helvetica', 20))  # layout must be a list of lists
 
-event, values = window.read()
-print(event)
-print(values)
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case 'Add':
+            todos = functions.read_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
+
 window.close()
