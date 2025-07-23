c1cb47c89e4fa641808dec8e3b0dc9e77d424044
Hrushikesh <hrushigkesh@gmail.com>
2025-07-23 07:40:31 +0530

    Create columns, image and description

--- app2.portfolio/main.py
+++ app2.portfolio/main.py
@@ -1,9 +1,11 @@
 import streamlit as st
 
+st.set_page_config(layout="wide")
+
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("")
+    st.image("images/myphoto.jpg")
 
 with col2:
     st.title("Hrushikesh G")
@@ -11,4 +13,4 @@ with col2:
     Hi, I am Hrushikesh G! I am a python programmer and a student pursuing a Diploma in Computer Science at Dayananda Sagar Institute of Technology.
     I am doing this Python Certification to Master Python and become a Python Developer.
     """
-    st.write(content)
+    st.info(content)
